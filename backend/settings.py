c438a1943ac03544c927bb51fea0f0008e12fe3a
VilosmZX <jravaell@gmail.com>
2022-07-25 16:49:04 +0700

    a

--- backend/settings.py
+++ backend/settings.py
@@ -168,8 +168,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://localhost:3000'
+    'https://tarkishop.netlify.app',
 ]
 
 MEDIA_URL = '/media/'
