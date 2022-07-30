99c96a2d2b7ec80c7fec63991fccf158f0b963a7
VilosmZX <jravaell@gmail.com>
2022-07-30 12:42:26 +0700

    ~

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.http import JsonResponse
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
@@ -8,9 +9,20 @@ from rest_framework_simplejwt.views import (
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import MenuSerializer, Menu
 from rest_framework import status
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import storage
+import pyrebase
+from requests.utils import requote_uri
+
+
+config = {
+    'apiKey': "AIzaSyBAMLQ1dz5oblsp0fRhimciOBC6HY_ES84",
+    'authDomain': "myweb-59a0a.firebaseapp.com",
+    'projectId': "myweb-59a0a",
+    'storageBucket': "myweb-59a0a.appspot.com",
+    'messagingSenderId': "324236950024",
+    'appId': "1:324236950024:web:8ca9dd3b7872427f21afbd",
+    'measurementId': "G-3F334QY1NB",
+    'databaseURL': '',
+}
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -41,9 +53,19 @@ def getMenu(request):
 
 @api_view(['POST'])
 def addMenu(request): 
+    firebase = pyrebase.initialize_app(config)
+    generated_id = uuid4()
+    image_name = f'image/food_{request.data["title"]}_{generated_id}'
+    storage = firebase.storage()
+    data = storage.child(image_name).put(request.data['image'])
+    image_url = storage.child(image_name).get_url(data['downloadTokens'])
+    image_url = requote_uri(image_url)
     menu = MenuSerializer(data=request.data)
+    request.data['image'] = image_url
     if menu.is_valid():
-        menu.save()
+        
+        menu.validated_data['id'] = generated_id
+        obj = menu.save()
         return Response(menu.data, status=status.HTTP_201_CREATED)
     else:
         print('error', menu.errors)
--- base/models.py
+++ base/models.py
@@ -1,11 +1,13 @@
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import User
 
 class Menu(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, null=False, blank=False, editable=False)
     owner = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     price = models.FloatField()
-    image = models.ImageField(upload_to='media/menus')
+    image = models.CharField(unique=True, max_length=300)
 
     
     def __str__(self):
